de168b2beeda20a286868736e490caf789168de0
ikramiste <ikrame.saadi@gmail.com>
2023-12-04 13:48:23 +0100

    gem letter_opener_web for email display in development  environnement added - emails can be consulted in http://localhost:3000/letter_opener

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   resources :reviews
   resources :rentals
   resources :bicycles
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: '/letter_opener'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
