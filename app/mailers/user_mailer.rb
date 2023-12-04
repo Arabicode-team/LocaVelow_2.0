f8689c33de6544214edffa6a3c87ba84b8df04a5
ikramiste <ikrame.saadi@gmail.com>
2023-12-04 13:19:01 +0100

    added url in usermailer compatible regardless of https

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
     def welcome_email(user)
         @user = user 
     
-=        @url  = 'https://locavelow-70268798a3e1.herokuapp.com/' 
+       @url  = 'http://locavelow-70268798a3e1.herokuapp.com/' 
     
         mail(to: @user.email, subject: 'Bienvenue chez Locavelow !') 
       end
