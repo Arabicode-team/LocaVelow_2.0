5199410d966894a1e8ea4a8baa325fc0c1591d0d
ikramiste <ikrame.saadi@gmail.com>
2023-12-06 17:42:54 +0100

    set default timezone of app to be paris france

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,9 @@ module LocaVelow20
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = "Paris"
 
     config.active_job.queue_adapter = :async
 
