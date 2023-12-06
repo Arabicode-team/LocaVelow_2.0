a4e0a42dd1e6ccbb0748e459d733e37f5ed87355
ikramiste <ikrame.saadi@gmail.com>
2023-12-06 17:35:32 +0100

    update of rental status is now fully functional in rails console when performed manually - job scheduled to be performed every 15m for now

--- app/jobs/update_rental_status_job.rb
+++ app/jobs/update_rental_status_job.rb
@@ -1,9 +1,11 @@
 class UpdateRentalStatusJob < ApplicationJob
   queue_as :default
 
-  def perform
+  def perform(*args)
     Rails.logger.info("UpdateRentalStatusJob started at #{Time.now}")
-    Rental.update_status_for_past_rentals
+    Rental.all.each do |rental|
+      rental.update_status_for_past_rentals
+    end
     Rails.logger.info("UpdateRentalStatusJob finished at #{Time.now}")
   end
 end
